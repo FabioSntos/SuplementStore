68e47f3342db890d461ffae32157ae8b8cb82e4f
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-07-28 11:00:43 -0300

    feat: return products by id

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,21 @@ import AdminUserController from "./app/controller/AdminUserController";
 import SessionController from "./app/controller/SessionController";
 import ProductPagesController from "./app/controller/ProductPagesController";
 
-import multer from "multer";
-import multerConfig from "./config/multer";
-
 import authMiddleware from "./app/middlewares/auth";
 import DepartmentController from "./app/controller/DepartmentController";
 
 const routes = new Router();
 
+//login routes
 routes.post("/admin", AdminUserController.store);
 routes.post("/session", SessionController.session);
+
+//products routes
 routes.post("/products", ProductPagesController.store);
+routes.get("/products/:id", ProductPagesController.ProductById);
+routes.get("/products", ProductPagesController.returnProducts);
+
+//departments routes
 routes.get("/departments", DepartmentController.returnDept);
 routes.post("/departments", DepartmentController.store);
 
