5cb3b32a5be092b60a1ca15ff1b90da0371dcae8
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-08-09 19:49:50 -0300

    feat: database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "docker",
-  database: "sistema",
+  host: "ec2-44-194-112-166.compute-1.amazonaws.com",
+  username: "wyvkqsycbhygsy",
+  password: "b3309c5ed2fd421aec509fef86ff229679f5517893a912e72814e550d38f5ba7",
+  database: "d5kq5gccvpf2en",
   define: {
     timestamps: true,
     underscored: true,
