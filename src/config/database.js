43058eaa637f0d9a1e00bea4a20fe4fab331bb26
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-08-10 13:19:48 -0300

    fix: deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
 module.exports = {
   dialect: "postgres",
   url: process.env.DATABASE_URL,
+  ssl: true,
+  extra: { ssl: { rejectUnauthorized: false } },
   define: {
     timestamps: true,
     underscored: true,
