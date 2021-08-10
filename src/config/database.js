39ed43a1f00361f985ef8d4da9cbf790ad172fcc
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-08-10 13:03:37 -0300

    fix: deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,6 @@
 module.exports = {
   dialect: "postgres",
-  host: "ec2-44-194-112-166.compute-1.amazonaws.com",
-  username: "wyvkqsycbhygsy",
-  password: "b3309c5ed2fd421aec509fef86ff229679f5517893a912e72814e550d38f5ba7",
-  database: "d5kq5gccvpf2en",
+  url: process.env.DATABASE_URL,
   define: {
     timestamps: true,
     underscored: true,
