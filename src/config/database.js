bfad42d709be0ec85f274d1cb8467a3f81d22dad
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-08-10 14:14:41 -0300

    fix: imports

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,7 @@
 module.exports = {
   dialect: "postgres",
   url: process.env.DATABASE_URL,
-  ssl: true,
-  extra: { ssl: { rejectUnauthorized: false } },
+
   define: {
     timestamps: true,
     underscored: true,
